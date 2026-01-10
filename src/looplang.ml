c45e1d3ed74f802bb50a631ce5dfcda4d337cdf6
jessicapeck <jess@pecksplace.com>
2026-01-10 11:43:28 +0000

    Convert 2 space tabs to 4 space tabs

--- src/looplang.ml
+++ src/looplang.ml
@@ -4,40 +4,40 @@ open Ast
 let tokens = ref []
 
 let log_lexer lexer_func lexbuf =
-  let token = lexer_func lexbuf in
-  tokens := token :: !tokens;
-  token
+    let token = lexer_func lexbuf in
+    tokens := token :: !tokens;
+    token
 
 let () =
-  let filename = Sys.argv.(1) in
-  let channel = open_in filename in
-  let lexbuf = Lexing.from_channel channel in
+    let filename = Sys.argv.(1) in
+    let channel = open_in filename in
+    let lexbuf = Lexing.from_channel channel in
 
-  let debug_lexer = log_lexer Lexer.next_token in
-  try
-    let ast = Parser.pattern debug_lexer lexbuf in
-    (* print_endline (Ast.string_of_pattern ast) *)
+    let debug_lexer = log_lexer Lexer.next_token in
+    try
+        let ast = Parser.pattern debug_lexer lexbuf in
+        (* print_endline (Ast.string_of_pattern ast) *)
 
-    let initial_env = [] in
-    let _ = Type_checker.check_pattern initial_env ast in
-    Printf.printf("Type checking: SUCCESS\n")
-  with
-  | Parser.Error ->
-      let pos = lexbuf.lex_curr_p in
-      Printf.eprintf "Syntax error at line %d, column %d\n"
-        pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1);
+        let initial_env = [] in
+        let _ = Type_checker.check_pattern initial_env ast in
+        Printf.printf("Type checking: SUCCESS\n")
+    with
+    | Parser.Error ->
+        let pos = lexbuf.lex_curr_p in
+        Printf.eprintf "Syntax error at line %d, column %d\n"
+            pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1);
 
-      Printf.eprintf "--- Tokens seen up to error ---\n";
-      List.iter (fun token ->
-        Printf.eprintf "%s\n" (Lexer.string_of_token token)
-      ) (List.rev !tokens);
-      exit 1
-  | Type_checker.TypeError msg ->
-      Printf.eprintf "Type error: %s\n" msg;
-      exit 1
-  | Type_checker.ArgError msg ->
-      Printf.eprintf "Argument error: %s\n" msg;
-      exit 1
-  | Failure msg ->
-      Printf.eprintf "Error: %s\n" msg;
-      exit 1
+        Printf.eprintf "--- Tokens seen up to error ---\n";
+        List.iter (fun token ->
+            Printf.eprintf "%s\n" (Lexer.string_of_token token)
+        ) (List.rev !tokens);
+        exit 1
+    | Type_checker.TypeError msg ->
+        Printf.eprintf "Type error: %s\n" msg;
+        exit 1
+    | Type_checker.ArgError msg ->
+        Printf.eprintf "Argument error: %s\n" msg;
+        exit 1
+    | Failure msg ->
+        Printf.eprintf "Error: %s\n" msg;
+        exit 1
--- test/unit_tests.ml
+++ test/unit_tests.ml
@@ -4,80 +4,81 @@ open Ast
 
 
 let read_file filename =
-  let channel = open_in filename in
-  let length = in_channel_length channel in
-  let contents = really_input_string channel length in
-  close_in channel;
-  contents
-
-
-let convert_to_ast filename =
-  let channel = open_in filename in
-  let lexbuf = Lexing.from_channel channel in
-  let ast = Parser.pattern Lexer.next_token lexbuf in
-  close_in channel;
-  Ast.string_of_pattern ast
+    let channel = open_in filename in
+    let length = in_channel_length channel in
+    let contents = really_input_string channel length in
+    close_in channel;
+    contents
 
 
 let convert_to_token_stream filename =
-  let channel = open_in filename in
-  let lexbuf = Lexing.from_channel channel in
-  let rec aux acc =
-    let token = Lexer.next_token lexbuf in
-    match token with
-    | Parser.EOF -> List.rev (Lexer.string_of_token token :: acc)
-    | _ -> aux (Lexer.string_of_token token :: acc)
-  in
-  let tokens = aux [] in
-  close_in channel;
-  tokens
+    let channel = open_in filename in
+    let lexbuf = Lexing.from_channel channel in
+    let rec aux acc =
+        let token = Lexer.next_token lexbuf in
+        match token with
+        | Parser.EOF -> List.rev (Lexer.string_of_token token :: acc)
+        | _ -> aux (Lexer.string_of_token token :: acc)
+    in
+    let tokens = aux [] in
+    close_in channel;
+    tokens
+
+
+let convert_to_ast filename =
+    let channel = open_in filename in
+    let lexbuf = Lexing.from_channel channel in
+    let ast = Parser.pattern Lexer.next_token lexbuf in
+    close_in channel;
+    Ast.string_of_pattern ast
 
 
 (* list of (test_name, filename) pairs *)
 let tests = [
-  ("Empty pattern", "empty_pattern");
-  ("Empty pattern (with newlines)", "empty_pattern_with_newlines");
-  ("Single row", "single_row");
-  ("Row indicators", "row_indicators");
-  ("Stitch multipliers", "stitch_multipliers");
-  ("Integer variables", "integer_variables");
-  ("Function definition", "function_definition");
-  ("Multiple line function return", "multiple_line_function_return");
-  ("If-else statement", "if_else_statement");
-  ("If statement (no else)", "if_statement");
-  ("Defining variables (all types)", "variable_definitions");
-  ("Passing a stitch sequence argument", "stitch_seq_arg");
-  ("Function call within a stitch sequence", "function_call_seq_item")
+    ("Empty pattern", "empty_pattern");
+    ("Empty pattern (with newlines)", "empty_pattern_with_newlines");
+    ("Single row", "single_row");
+    ("Row indicators", "row_indicators");
+    ("Stitch multipliers", "stitch_multipliers");
+    ("Integer variables", "integer_variables");
+    ("Function definition", "function_definition");
+    ("Multiple line function return", "multiple_line_function_return");
+    ("If-else statement", "if_else_statement");
+    ("If statement (no else)", "if_statement");
+    ("Defining variables (all types)", "variable_definitions");
+    ("Passing a stitch sequence argument", "stitch_seq_arg");
+    ("Function call within a stitch sequence", "function_call_seq_item")
 ]
 
 
-let create_ast_test (test_name, filename) = 
-  let test_fn () =
-    let expected_ast = read_file ("./test/parser_results/" ^ filename ^ ".ast") in
-    let actual_ast = convert_to_ast ("./test/patterns/" ^ filename ^ ".txt") in
-    Alcotest.(check string) test_name expected_ast actual_ast
-  in
-  Alcotest.test_case test_name `Quick test_fn
-
-let ast_test_suite =
-  List.map create_ast_test tests
-
-
 let create_token_stream_test (test_name, filename) = 
-  let test_fn () =
-    let expected_token_stream = String.split_on_char '\n' (read_file ("./test/lexer_results/" ^ filename ^ ".tokens")) in
-    let actual_token_stream = convert_to_token_stream ("./test/patterns/" ^ filename ^ ".txt") in
-    Alcotest.(check (list string)) test_name expected_token_stream actual_token_stream
-  in
-  Alcotest.test_case test_name `Quick test_fn
+    let test_fn () =
+        let expected_token_stream = String.split_on_char '\n' (read_file ("./test/lexer_results/" ^ filename ^ ".tokens")) in
+        let actual_token_stream = convert_to_token_stream ("./test/patterns/" ^ filename ^ ".txt") in
+        Alcotest.(check (list string)) test_name expected_token_stream actual_token_stream
+    in
+    Alcotest.test_case test_name `Quick test_fn
 
 let token_stream_test_suite =
-  List.map create_token_stream_test tests
+    List.map create_token_stream_test tests
+
+
+let create_ast_test (test_name, filename) = 
+    let test_fn () =
+        let expected_ast = read_file ("./test/parser_results/" ^ filename ^ ".ast") in
+        let actual_ast = convert_to_ast ("./test/patterns/" ^ filename ^ ".txt") in
+        Alcotest.(check string) test_name expected_ast actual_ast
+    in
+    Alcotest.test_case test_name `Quick test_fn
+
+let ast_test_suite =
+    List.map create_ast_test tests
 
 
 let () =
-  let test_suites = [
-    ("Pattern -> AST Conversion Test", ast_test_suite);
-    ("Pattern -> Token Stream Conversion Test", token_stream_test_suite)
-  ] in
-  run "LoopLang Compiler" test_suites
+    let test_suites = [
+        ("Pattern -> Token Stream Conversion Test", token_stream_test_suite)
+        ("Pattern -> AST Conversion Test", ast_test_suite);
+    ] in
+    run "LoopLang Compiler" test_suites
+
