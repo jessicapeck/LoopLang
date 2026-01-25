963c95dbc322414790a988e75345adf79cb01e60
jessicapeck <jess@pecksplace.com>
2026-01-25 15:41:37 +0000

    Ensure safety in closing channels when exceptions are raised

--- test/test_utils.ml
+++ test/test_utils.ml
@@ -4,30 +4,42 @@ open Ast
 
 let convert_to_token_stream filename =
     let channel = open_in filename in
-    let lexbuf = Lexing.from_channel channel in
-    let rec aux acc =
-        let token = Lexer.next_token lexbuf in
-        match token with
-        | Parser.EOF -> List.rev (Lexer.string_of_token token :: acc)
-        | _ -> aux (Lexer.string_of_token token :: acc)
-    in
-    let tokens = aux [] in
-    close_in channel;
-    tokens
+    try
+        let lexbuf = Lexing.from_channel channel in
+        let rec aux acc =
+            let token = Lexer.next_token lexbuf in
+            match token with
+            | Parser.EOF -> List.rev (Lexer.string_of_token token :: acc)
+            | _ -> aux (Lexer.string_of_token token :: acc)
+        in
+        let tokens = aux [] in
+        close_in channel;
+        tokens
+    with e ->
+        close_in channel;
+        raise e
 
 
 let convert_to_ast filename =
     let channel = open_in filename in
-    let lexbuf = Lexing.from_channel channel in
-    let ast = Parser.pattern Lexer.next_token lexbuf in
-    close_in channel;
-    Ast.string_of_pattern ast
+    try
+        let lexbuf = Lexing.from_channel channel in
+        let ast = Parser.pattern Lexer.next_token lexbuf in
+        close_in channel;
+        Ast.string_of_pattern ast
+    with e ->
+        close_in channel;
+        raise e
 
 
 let run_type_checker filename =
     let channel = open_in filename in
-    let lexbuf = Lexing.from_channel channel in
-    let ast = Parser.pattern Lexer.next_token lexbuf in
-    close_in channel;
-    let _ = Type_checker.check_pattern ast in
-    ()
+    try
+        let lexbuf = Lexing.from_channel channel in
+        let ast = Parser.pattern Lexer.next_token lexbuf in
+        close_in channel;
+        let _ = Type_checker.check_pattern ast in
+        ()
+    with e ->
+        close_in channel;
+        raise e
